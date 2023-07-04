a42dc154a13645b89f9af16485db6d4db5260a20
Selvi <selviam02@gmail.com>
2023-07-04 17:31:22 +0300

    added footer, home and contact UI

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,22 @@
+import Hero from '/public/hero.png';
 import Image from 'next/image';
+import Button from '@/components/button/Button';
 import styles from './page.module.css';
 
 export default function Home() {
-	return <main>Home</main>;
+	return (
+		<div className={styles.container}>
+			<div className={styles.item}>
+				<h1 className={styles.title}>Better design for your digital products.</h1>
+				<p className={styles.desc}>
+					Turning your Idea into Reality. We bring together the teams from the global tech
+					industry.
+				</p>
+				<Button url={'/'} text={'Works'}></Button>
+			</div>
+			<div className={styles.item}>
+				<Image src={Hero} alt='' className={styles.img} />
+			</div>
+		</div>
+	);
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,8 @@
+import Image from 'next/image';
+import Facebook from '/public/1.png';
+import Instagram from '/public/2.png';
+import Twitter from '/public/3.png';
+import Youtube from '/public/4.png';
 import styles from './footer.module.css';
 import React from 'react';
 
@@ -5,7 +10,12 @@ const Footer = () => {
 	return (
 		<div className={styles.container}>
 			<div>&copy;2023 Selvi Amparoz. All rights reserved.</div>
-			<div>hehe</div>
+			<div className={styles.socials}>
+				<Image src={Facebook} width={15} height={15} alt='facebook'></Image>
+				<Image src={Instagram} width={15} height={15} alt='facebook'></Image>
+				<Image src={Twitter} width={15} height={15} alt='facebook'></Image>
+				<Image src={Youtube} width={15} height={15} alt='facebook'></Image>
+			</div>
 		</div>
 	);
 };
